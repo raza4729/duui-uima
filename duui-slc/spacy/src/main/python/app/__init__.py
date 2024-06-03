1f0b26d413f21c25f2a6f63e017d334e56a0a1c4
Manuel Stoeckel <manuel.stoeckel@em.uni-frankfurt.de>
2024-06-03 18:15:04 +0200

    refactor!(slc): Replaced duplicate python files with symlinks

--- duui-slc/spacy/src/main/python/app/__init__.py
+++ duui-slc/spacy/src/main/python/app/__init__.py
@@ -1,6 +1,1 @@
-import logging
-
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-)
+../../../../../base/src/main/python/app/__init__.py

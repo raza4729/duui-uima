da306690b6d9a3ccdfccde32d89619a37de1509b
Manuel Stoeckel <manuel.stoeckel@em.uni-frankfurt.de>
2024-06-04 16:18:42 +0200

    minor(slc): Removed spacy type hints from utils.py

--- duui-slc/base/src/main/python/app/utils.py
+++ duui-slc/base/src/main/python/app/utils.py
@@ -2,8 +2,6 @@ import logging
 from dataclasses import dataclass, field
 from typing import Final
 
-from spacy.tokens import Token
-
 logger = logging.getLogger(__name__)
 
 
@@ -14,17 +12,17 @@ EOS_MARKERS: Final[set[str]] = set(".?!")
 class TokenToId:
     mapping: dict[int, int] = field(default_factory=dict)
 
-    def __getitem__(self, token: Token):
+    def __getitem__(self, token):
         return self.mapping[hash(token)]
 
-    def __setitem__(self, token: Token, value: int):
+    def __setitem__(self, token, value: int):
         if hash(token) in self.mapping:
             raise ValueError(f"Token {token} already exists in the mapping.")
         if value != len(self.mapping):
             raise ValueError(f"Value {value} is not the next value in the mapping.")
         self.mapping[hash(token)] = value
 
-    def add(self, token: Token) -> int:
+    def add(self, token) -> int:
         _hash = hash(token)
         self.mapping[_hash] = len(self.mapping)
         return self.mapping[_hash]
